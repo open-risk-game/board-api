ce234e9bec706d29ea2e6cb68f74e49b50123095
Adam Blackwater <me@adamblackwater.com>
2020-06-27 18:20:55 +0100

    Return x and y coords of a tile
    The SQL query no longer joins tables as the data returned was not
    necessary.

--- models/board.py
+++ models/board.py
@@ -5,12 +5,10 @@ from aiohttp import web
 async def get(request):
     params = request.rel_url.query
     board_id = params['board_id']
-    print(board_id)
     query = f'''
-        SELECT player.username, tile.id, tile.tokens
-        FROM tiles as tile
-        INNER JOIN players as player ON player.id = tile.owner
-        WHERE tile.boardid = {board_id}
+        SELECT owner as player_id, tokens, x, y
+        FROM tiles
+        WHERE boardid = {board_id}
     '''
     async with request.app['pool'].acquire() as db_conn:
         cursor = await db_conn.cursor(aiomysql.DictCursor)
