dedf5d33cd70fbb716dbc7ea9fc2601d0d530fc2
Adam Blackwater <me@adamblackwater.com>
2020-07-31 16:18:51 +0100

    Move to using 'pool' fixture defined in conftest.py

--- tests/test_hex.py
+++ tests/test_hex.py
@@ -1,25 +1,5 @@
-import os
 import json
-import pytest
-import aiomysql
 import models.hexagon as hexagon
-
-DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = os.environ.get('DB_PORT')
-DB_USER = os.environ.get('DB_USER')
-DB_PASS = os.environ.get('DB_PASS')
-DB_NAME = os.environ.get('DB_NAME')
-
-
-@pytest.fixture
-async def pool(loop):
-    async with aiomysql.create_pool(
-            host=DB_HOST,
-            port=int(DB_PORT),
-            user=DB_USER,
-            password=DB_PASS,
-            db=DB_NAME) as pool:
-        yield pool
 
 
 class FakeURL:
